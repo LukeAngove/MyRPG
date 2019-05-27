8b25222f15489681131c9e3cfbe7c83184ebc872
Luke Angove <luke.angove@gmail.com>
2019-05-28 01:08:57 +1000

    Add support for '0' min modifiers in monster overlays
    - Fix bug in join, now we make a new card, don't modify old one
    - Initial support looking forward for multiple modifiers

--- card_maker/src/card_maker/cmd/monster_cards.go
+++ card_maker/src/card_maker/cmd/monster_cards.go
@@ -15,6 +15,23 @@ type MonsterCard struct {
 	Flavor    string
 }
 
+func NewMonsterCard() MonsterCard {
+	card := MonsterCard{}
+	card.Tags = []string{}
+	card.Attributes = map[string]int{
+		"L": 0,
+		"F": 0,
+		"E": 0,
+		"W": 0,
+		"S": 0,
+	}
+	card.Rules = map[string]string{}
+	card.Abilities = []map[string]string{}
+	card.Flavor = ""
+
+	return card
+}
+
 type CardModifierHolder struct {
 	MaxInclude int `yaml:"max_include"`
 	MinInclude int `yaml:"min_include"`
@@ -31,19 +48,28 @@ type MonsterCards struct {
 	Cards     []MonsterCard
 	Rules     []string          `yaml:"rules_order"`
 	Abilities []string          `yaml:"abilities_order"`
+	Overlays  []string          `yaml:"overlay_order"`
 }
 
 func join(a MonsterCard, b MonsterCard) MonsterCard {
-	res := MonsterCard{}
+	res := NewMonsterCard()
 
 	res.Title = a.Title + " " + b.Title
 
-	res.Attributes = a.Attributes
+	for key, _ := range a.Attributes {
+		res.Attributes[key] += a.Attributes[key]
+	}
 	for key, _ := range b.Attributes {
 		res.Attributes[key] += b.Attributes[key]
 	}
 
-	res.Rules = a.Rules
+	for key, val := range a.Rules {
+		if src_val, ok := res.Rules[key]; ok {
+			res.Rules[key] = src_val + val
+		} else {
+			res.Rules[key] = val
+		}
+	}
 	for key, val := range b.Rules {
 		if src_val, ok := res.Rules[key]; ok {
 			res.Rules[key] = src_val + val
@@ -52,8 +78,11 @@ func join(a MonsterCard, b MonsterCard) MonsterCard {
 		}
 	}
 
-	res.Abilities = append(a.Abilities, b.Abilities...)
-	res.Tags = append(a.Tags, b.Tags...)
+	res.Abilities = append(res.Abilities, a.Abilities...)
+	res.Abilities = append(res.Abilities, b.Abilities...)
+
+	res.Tags = append(res.Tags, a.Tags...)
+	res.Tags = append(res.Tags, b.Tags...)
 
 	return res
 }
@@ -80,32 +109,53 @@ func getMultiplier(mod CardModifierHolder) int {
 	return res
 }
 
-func genCombinations(mod CardModifierHolder) CardModifierHolder {
-	return mod
-}
+func genCombinations(mod CardModifierHolder) map[string]MonsterCard {
+	mods := mod.Modifiers
 
-func generateCardsFromTemplate(generator CardGenerator) []MonsterCard{
-	res := []MonsterCard{}
-
-	this_card := generator.Base
-
-	for _, overlay := range generator.Overlays {
-		keys := make([]string, 0, len(overlay.Modifiers))
-		for k := range overlay.Modifiers {
-			keys = append(keys, k)
-		}
-
-		this_card = join(this_card, overlay.Modifiers[keys[0]])
-		this_card.Title += " " + keys[0]
+	if mod.MinInclude == 0 {
+		mods[""] = MonsterCard{}
 	}
 
+	return mod.Modifiers
+}
+
+func applyOverlay(base_card MonsterCard, overlays CardModifierHolder) []MonsterCard {
+	res := []MonsterCard{}
+
+	modifiers := genCombinations(overlays)
+
+	for key, overlay := range modifiers {
+		new_card := join(base_card, overlay)
+		new_card.Title += " " + key
+		res = append(res, new_card)
+	}
+
+	return res
+}
+
+func generateCardsFromTemplate(cards MonsterCards) []MonsterCard {
+	res := []MonsterCard{}
+	generator := cards.Generator
+
+	this_card := generator.Base
 	res = append(res, this_card)
+
+	for _, overlay_type := range cards.Overlays {
+		overlay := generator.Overlays[overlay_type]
+
+		new_res := []MonsterCard{}
+		for _, card := range res {
+			new_res = append(new_res, applyOverlay(card, overlay)...)
+		}
+
+		res = new_res
+	}
 
 	return res
 }
 
 func (cards *MonsterCards) Preprocess() {
-	(*cards).Cards = generateCardsFromTemplate((*cards).Generator)
+	(*cards).Cards = generateCardsFromTemplate(*cards)
 }
 
 func (cards *MonsterCards) Load(yaml_bytes []byte) {
