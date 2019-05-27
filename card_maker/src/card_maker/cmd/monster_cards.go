19b07847f9b8fa66ac28d950ad060fd23cff48ae
Luke Angove <luke.angove@gmail.com>
2019-05-27 23:26:09 +1000

    Add monster overlays, refactor go code
    - Move details into seperate package
    - Refactor interfaces to OO design
    - Add overlays; monsters can be built from multiple layered types

--- card_maker/src/card_maker/cmd/monster_cards.go
+++ card_maker/src/card_maker/cmd/monster_cards.go
@@ -1,49 +1,120 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"text/template"
-
+	"cardgenerator"
 	"gopkg.in/yaml.v2"
 )
 
 type MonsterCard struct {
 	Title     string
 	Image     string
-	Cost      string
+	Tags      []string
+	Attributes map[string]int
 	Rules     map[string]string
 	Abilities []map[string]string
 	Flavor    string
 }
 
-type Cards struct {
-	Cards     []MonsterCard     `yaml:"cards"`
+type CardModifierHolder struct {
+	MaxInclude int `yaml:"max_include"`
+	MinInclude int `yaml:"min_include"`
+    Modifiers map[string]MonsterCard `yaml:"items"`
+}
+
+type CardGenerator struct {
+    Base MonsterCard `yaml:"base"`
+	Overlays map[string]CardModifierHolder `yaml:"overlays"`
+}
+
+type MonsterCards struct {
+	Generator CardGenerator   `yaml:"generator"`
+	Cards     []MonsterCard
 	Rules     []string          `yaml:"rules_order"`
 	Abilities []string          `yaml:"abilities_order"`
-	Colors    map[string]string `yaml:"colors"`
 }
 
-func makeMonsterCards(cards Cards, template *template.Template, output string) {
-	file, err := os.Create(output)
-	check(err)
-	err = template.Execute(file, cards)
-	check(err)
-}
+func join(a MonsterCard, b MonsterCard) MonsterCard {
+	res := MonsterCard{}
 
-func makeCardsFromTemplate(yaml_desc string, html_template string, output string) {
-	paths := []string{
-		html_template,
+	res.Title = a.Title + " " + b.Title
+
+	res.Attributes = a.Attributes
+	for key, _ := range b.Attributes {
+		res.Attributes[key] += b.Attributes[key]
 	}
 
-	data, err := ioutil.ReadFile(yaml_desc)
-	check(err)
+	res.Rules = a.Rules
+	for key, val := range b.Rules {
+		if src_val, ok := res.Rules[key]; ok {
+			res.Rules[key] = src_val + val
+		} else {
+			res.Rules[key] = val
+		}
+	}
 
-	cards := Cards{}
-	err = yaml.Unmarshal(data, &cards)
-	check(err)
+	res.Abilities = append(a.Abilities, b.Abilities...)
+	res.Tags = append(a.Tags, b.Tags...)
 
-	t := template.Must(template.ParseFiles(paths...))
+	return res
+}
 
-	makeMonsterCards(cards, t, output)
+func factorial(n int) int {
+	res := 1
+
+	for i := 1; i<=n; i++ {
+		res *= i
+	}
+
+	return res
+}
+
+func combinations(n int, r int) int {
+	return factorial(n) / (factorial(r) * factorial(n-r))
+}
+
+func getMultiplier(mod CardModifierHolder) int {
+	res := 0
+	for i := mod.MinInclude; i <= mod.MaxInclude; i++ {
+		res += combinations(i, len(mod.Modifiers))
+	}
+	return res
+}
+
+func genCombinations(mod CardModifierHolder) CardModifierHolder {
+	return mod
+}
+
+func generateCardsFromTemplate(generator CardGenerator) []MonsterCard{
+	res := []MonsterCard{}
+
+	this_card := generator.Base
+
+	for _, overlay := range generator.Overlays {
+		keys := make([]string, 0, len(overlay.Modifiers))
+		for k := range overlay.Modifiers {
+			keys = append(keys, k)
+		}
+
+		this_card = join(this_card, overlay.Modifiers[keys[0]])
+		this_card.Title += " " + keys[0]
+	}
+
+	res = append(res, this_card)
+
+	return res
+}
+
+func (cards *MonsterCards) Preprocess() {
+	(*cards).Cards = generateCardsFromTemplate((*cards).Generator)
+}
+
+func (cards *MonsterCards) Load(yaml_bytes []byte) {
+	err := yaml.Unmarshal(yaml_bytes, cards)
+	cardgenerator.Check(err)
+	cards.Preprocess()
+}
+
+func main() {
+	cards := MonsterCards{}
+	cardgenerator.MakeCardsFromTemplate(&cards)
 }
